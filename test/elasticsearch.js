4353471b6ed60640e98778b73772e89b3ea08b3d
Simon Morley <simon@polkaspots.com>
2014-06-13 18:35:20 +0100

    Added aliases to ES

---
+++ test/elasticsearch.js
@@ -0,0 +1,54 @@
+var client = require('../lib/clients');
+var nas = require('../lib/nas');
+var es = require('../lib/elasticsearch');
+var stream = require('../lib/streams');
+var config = require('../lib/config');
+
+var elasticsearch = require('elasticsearch');
+var _client = new elasticsearch.Client({
+  host: config.es.host + ":" + config.es.port,
+  // log: 'trace'
+});
+
+
+describe("Elasticsearch", function(){
+
+  after(function(done){
+    _client.indices.exists({index: 'test'}, function(err,res) {
+      if (res == true) {
+        console.log("Deleted index")
+        _client.indices.delete({index: 'test'})
+      }
+    })
+    done();
+  });
+
+
+  it("should create an alias for es", function(done) {
+
+    var index_name = 'test'
+    es.create_aliases(index_name, function(alias_name) {
+      es.client.indices.getMapping({
+        index: alias_name,
+        // type: 'stream'
+      }, function(err,res) {
+        res[alias_name]["mappings"][alias_name]["properties"].should.not.equal([])
+        done();
+      })
+    })
+  })
+
+  it("should swap the mappings / index over", function(done) {
+
+    var index_name = 'test'
+    es.create_aliases(index_name, function(alias_name) {
+      console.log(alias_name)
+      es.swap_aliases(index_name, alias_name, function(res) {
+        console.log(res)
+        done();
+      })
+    })
+  })
+
+});
+
---
+++ test/polkaspots.js
@@ -0,0 +1,17 @@
+var ps = require('../lib/polkaspots');
+var config = require('./config-debug');
+
+describe("Polkaspots", function(){
+
+  it("return a cleaned mac", function(done){
+    ap_mac = '00:aa:22:33:44:55'
+
+    ps.clean_mac(ap_mac, function(doc) {
+      doc.should.equal('00-AA-22-33-44-55')
+      done()
+
+    })
+  })
+
+});
+
